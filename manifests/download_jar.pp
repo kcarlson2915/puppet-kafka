2027867fe9ed2bca91279bd0ffb78fbd3dce9aa8
kcarlson2915 <kcarlson@opentable.com>
2017-05-31 16:54:26 -0700

    Removing unneeded requirement

--- manifests/download_jar.pp
+++ manifests/download_jar.pp
@@ -2,6 +2,5 @@ define kafka::download_jar ($jar_name, $jar_source, $install_dir) {
     exec { "downloading ${jar_name}":
       command => "/usr/bin/wget -O /${install_dir}/${jar_name} ${jar_source}",
       creates => "/${install_dir}/${jar_name}",
-      require => File[$install_dir]
     }
 }
